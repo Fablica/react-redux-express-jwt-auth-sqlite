6fd82c821a00763a636687cee9fd1d859c681c56
Kento75 <kento2github@gmail.com>
2018-10-03 20:48:49 +0900

    Fix LoginPage backgound color

--- frontend/src/containers/LoginPage/LoginPage.jsx
+++ frontend/src/containers/LoginPage/LoginPage.jsx
@@ -46,7 +46,7 @@ class LoginPage extends Component {
     const { loggingIn } = this.props;
     const { username, password, submitted } = this.state;
     return (
-      <div>
+      <div className="ui inverted vertical masthead center aligned segment">
         <Grid
           textAlign="center"
           style={{ minHeight: 700 }}
